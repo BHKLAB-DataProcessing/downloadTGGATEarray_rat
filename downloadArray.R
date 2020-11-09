38f009a51f19369a06cd8384fa52348f31bfd269
imanthonny <34114220+imanthonny@users.noreply.github.com>
2020-11-09 17:02:14 -0500

    Update downloadArray.R

--- downloadArray.R
+++ downloadArray.R
@@ -3,6 +3,6 @@ library(curl)
 
 options(stringsAsFactors = FALSE)
 
-curl_download("https://orcestradata.blob.core.windows.net/toxico/TGGATES_rat_CELfiles.tar.gz", destfile = "/pfs/out/TGGATES_rat_CELfiles.tar.gz")
-untar("/pfs/out/TGGATES_rat_CELfiles.tar.gz", exdir = "/pfs/out/")
-file.remove("/pfs/out/TGGATES_rat_CELfiles.tar.gz")
+curl_download("https://orcestradata.blob.core.windows.net/toxico/TGGATES_rat_CEL.tar.gz", destfile = "/pfs/out/TGGATES_rat_CEL.tar.gz")
+untar("/pfs/out/TGGATES_rat_CEL.tar.gz", exdir = "/pfs/out/")
+file.remove("/pfs/out/TGGATES_rat_CEL.tar.gz")
