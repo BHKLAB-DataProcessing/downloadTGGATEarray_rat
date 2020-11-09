920f9c4f60295209397e91a5e7d3aaf68188e85f
imanthonny <34114220+imanthonny@users.noreply.github.com>
2020-11-09 09:58:11 -0500

    Update downloadArray.R

--- downloadArray.R
+++ downloadArray.R
@@ -2,28 +2,6 @@ require(downloader)
 library(curl)
 
 options(stringsAsFactors = FALSE)
-my.path="/pfs/out"
 
-dir1 <- "ftp://ftp.biosciencedbc.jp/archive/open-tggates/LATEST/Rat/in_vitro/"
-
-dir.create(file.path(my.path, "raw"), showWarnings=FALSE, recursive=TRUE)
-
-tt <- read.csv('https://orcestradata.blob.core.windows.net/toxico/TCG_Rat_array_samples_zip.txt') #all zip files
-
-samples <- tt$x
-
-tt <- split(samples, ceiling(seq_along(samples)/5))
-tt <- tt[[1]][1:2]
-
-for (i in 1:length(tt)) {
-
-print(i)
-#samples <- tt[[i]]
-samples <- tt[i]
-
-lapply(samples, function(filename){
-  curl_download(paste(dir1, filename, sep = ""), destfile = paste0(file.path(my.path, "raw"),"/",filename))
-  unzip(paste0(file.path(my.path, "raw"),"/",filename), exdir="/pfs/out")
-})
-  
-}
+curl_download("https://orcestradata.blob.core.windows.net/toxico/TGGATES_rat_CELfiles.tar.gz", destfile = "/pfs/out/TGGATES_rat_CELfiles.tar.gz")
+untar("/pfs/out/TGGATES_rat_CELfiles.tar.gz", exdir = "/pfs/out/")
